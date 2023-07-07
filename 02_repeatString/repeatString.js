1b222232473b042b557b056bf28ec88e91872649
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-07-07 15:55:44 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,30 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    //Create a variable to hold the string you're going to return
+    let returnString = "";
+
+    //Skips negative numbers
+    if(num<0){
+        return "ERROR";
+    }
+
+    if(string === ""){
+        return "";
+    }
+ 
+    //create a loop that repeats the given number of times
+    for(let i=0; i<num; i++){
+
+        if(num == 0){
+            break;
+        }
+
+        //add the given string to the result on each loop.
+        returnString+=string;
+
+    }
+
+    return returnString;
 
 };
 
