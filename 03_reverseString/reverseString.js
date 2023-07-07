d566194ef6eacf890fbdd48edc61d90ef5dbc3a9
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-07-07 16:19:29 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(inString) {
 
+    let reversedStr = "";
+
+    if(inString == ""){
+        return "";
+    }
+
+    for(let i=inString.length-1; i>=0; i--){
+        reversedStr+= inString[i];
+    }
+
+    return reversedStr;
 };
 
 // Do not edit below this line
