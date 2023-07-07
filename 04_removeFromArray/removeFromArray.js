a640c04061d2d846daae2afc74ff4cda375050ea
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-07-07 18:58:26 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    //convert all arguments to an array
+    //const args = Array.from(arguments);
+
+    return array.filter(x => !args.includes(x));
+
+    // //loops through array
+    // for (const i of args) {
+        
+    // }
 
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
