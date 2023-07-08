fe6a131b475201ce1adf4f0c57f732cbc4449a77
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-07-07 20:17:10 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  temp = (temp-32)*(5/9);
+
+  //round to 1 decimal
+  if(temp !== 0){
+    temp = parseFloat(temp.toFixed(1));
+  }
+  
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  temp = (temp*(9/5)+32);
+
+  //round to 1 decimal
+  if(temp !== 0){
+    temp = parseFloat(temp.toFixed(1));
+  }
+  
+  return temp;
 };
 
 // Do not edit below this line
