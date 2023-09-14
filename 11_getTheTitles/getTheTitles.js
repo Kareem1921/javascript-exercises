01e415ff22a479b6a1d68f4d818bd4c500b21308
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-09-13 23:38:46 -0400

    Complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let titles = [];
+
+    books.forEach(object => {
+        titles.push(object.title)
+    });
+
+    return titles;
 };
 
 // Do not edit below this line
