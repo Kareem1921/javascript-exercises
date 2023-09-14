dc5cf909cde2c0ee4a43c25e851b5d7bab64f1c1
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-09-13 23:23:40 -0400

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    // convert to number if argument is a string
+    if (typeof num === 'string' || num instanceof String){
+        num = Number(num)
+    }
+
+    // don't accept negatives
+    if(num < 0){
+        return "OOPS"
+    }
+
+    else if(num === 0){
+        return 0;
+    }
+
+    else if (num === 1){
+        return 1;
+    }
+
+    else{
+        return (fibonacci(num-1) + fibonacci(num-2));
+    }
+  
 
 };
 
