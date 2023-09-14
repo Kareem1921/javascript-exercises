73a191045c2c0a5f62640fdf80fd2da518699c52
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-09-13 23:02:52 -0400

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    //remove punction and spaces
+    let newStr = str.replace(/[.,:!?\s]/g,'');
+    newStr = newStr.toLowerCase();
+
+    //split into array 
+    let arr = newStr.split('');
+    let reversedArr = arr.reverse();
+
+    //convert reversed array to a reversed string
+    let reversedStr = reversedArr.join('');  
+
+    // check if they are the same
+    let result = newStr.localeCompare(reversedStr); //returning 0
+    return newStr.localeCompare(reversedStr) === 0 ? true : false;
 };
 
 // Do not edit below this line
