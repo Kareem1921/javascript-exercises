c6e304cedb60272f0c4895f9916db93f14146df0
Kareem1921 <104789675+Kareem1921@users.noreply.github.com>
2023-09-16 11:42:46 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,41 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // oldest should be the object that has the largest difference between birth year and death year
+
+    let oldest = people.reduce(function(oldestObject, currentObject) {
+        //let age = 0;
+        let oldestObjectAge = 0;
+
+        //check if object has a year of death
+        //if not, use current year to get age
+        if("yearOfDeath" in currentObject){
+            let age = currentObject.yearOfDeath - currentObject.yearOfBirth;
+
+            // get age of oldest 
+            "yearOfDeath" in oldestObject ? oldestObjectAge = oldestObject.yearOfDeath - oldestObject.yearOfBirth : oldestObjectAge = (new Date()).getFullYear()- oldestObject.yearOfBirth
+
+            // set oldest to current element if current element's age exceeds that of the recorded oldest person
+            if(age > oldestObjectAge){
+                oldestObject = currentObject
+            }
+           // age < oldestObject.yearOfDeath - oldestObject.yearOfBirth ? oldestObject = currentObject : false
+        }
+        else{
+            let age = (new Date()).getFullYear()- currentObject.yearOfBirth;
+
+            // get age of oldest 
+            "yearOfDeath" in oldestObject ? oldestObjectAge = oldestObject.yearOfDeath - oldestObject.yearOfBirth : oldestObjectAge = (new Date()).getFullYear()- oldestObject.yearOfBirth
+
+            if(age > oldestObjectAge){
+                oldestObject = currentObject
+            }
+        }
+
+        return oldestObject;
+
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
